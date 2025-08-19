e7ef3c582721e285be56541ecafd05adc41ab3d4
cracrazjf <cracrazjf@gmail.com>
2025-08-18 23:34:33 -0500

    prepare for release

--- psychai/config/__init__.py
+++ psychai/config/__init__.py
@@ -1,4 +1,5 @@
 from .settings import SettingsConfig
 from .training import TrainingConfig
+from .llm_training import LLMTrainingConfig
 
-__all__ = ["SettingsConfig", "TrainingConfig"]
+__all__ = ["SettingsConfig", "TrainingConfig", "LLMTrainingConfig"]
