fa9d8cef69bd3d6dd2e14644c27e82d9ed48cc47
cracrazjf <cracrazjf@gmail.com>
2025-09-10 00:46:35 -0500

    prepare for release

--- psychai/trainer/llm.py
+++ psychai/trainer/llm.py
@@ -4,7 +4,7 @@ from ..model_manager.llm import LLM_ModelManager
 import os
 from trl import SFTTrainer, SFTConfig
 
-class LLM_Trainer:
+class Trainer:
     def __init__(self, config):
         self.config = config
         self.model_manager = LLM_ModelManager()
